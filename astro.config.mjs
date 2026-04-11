f1a6b20ec1e6270078d7e752abb2c5204b819e43
Jocelyn212 <jocelyn.cf@gmail.com>
2026-04-11 20:57:14 +0200

    chore: configurar adapter de Astro para Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+const isVercel = process.env.VERCEL === '1';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
 
-  adapter: node({ mode: 'standalone' }),
+  adapter: isVercel ? vercel() : node({ mode: 'standalone' }),
 
   vite: {
     plugins: [tailwindcss()]
